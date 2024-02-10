a567e067cb33a7c33b185087fc4fd2d4231b614d
pabloarshid <umarsa.birvancity@gmail.com>
2024-02-09 23:57:37 -0800

    updated the graphs 2/9 done

--- tyresehaliburton/app.py
+++ tyresehaliburton/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from flask_migrate import Migrate  # Import Flask-Migrate
 import plotly.express as px
 import pandas as pd
@@ -8,7 +9,118 @@ from extensions import db  # Import the SQLAlchemy instance
 from nba_api.stats.endpoints import leagueleaders, playergamelog
 from datetime import datetime
 import numpy as np
+# -----------Fetch Functions-----------------
+def fetch_current_season_assist_leaders():
+    current_year = datetime.now().year
+    season_str = f"{current_year-1}-{str(current_year)[-2:]}"
 
+    # Assuming you have a Season table where the year is stored as "YYYY-YY"
+    season = Season.query.filter_by(year=season_str).first()
+    if not season:
+        return []
+
+    assist_leaders = db.session.query(
+        Player.player_name,
+        func.sum(GameLog.ast).label('total_assists')
+    ).join(SeasonStats, SeasonStats.player_id == Player.id)\
+     .join(GameLog, GameLog.season_stats_id == SeasonStats.id)\
+     .filter(SeasonStats.season_id == season.id)\
+     .group_by(Player.player_name)\
+     .order_by(func.sum(GameLog.ast).desc())\
+     .limit(20)\
+     .all()
+
+    return assist_leaders
+def fetch_top_10_players_avg_assists_2023_24():
+    season_str = "2023-24"  # Define the season string
+
+    # Query to fetch top 10 players' names and their average points for the 2023-24 season
+    top_players_avg_assists = db.session.query(
+        Player.player_name,
+        (SeasonStats.assists).label('avg_assists_per_game')
+    ).join(SeasonStats, SeasonStats.player_id == Player.id)\
+     .join(Season, SeasonStats.season_id == Season.id)\
+     .filter(Season.year == season_str)\
+     .order_by((SeasonStats.assists).desc())\
+     .limit(10)\
+     .all()
+
+    return top_players_avg_assists
+
+    game_logs = []
+    for player in top_10_players:
+        player_id = player.SeasonStats.player_id
+        season_stats = SeasonStats.query.filter_by(player_id=player_id, season_id=season.id).first()
+        
+        if season_stats:
+            player_game_logs = GameLog.query.filter_by(season_stats_id=season_stats.id).all()
+            for log in player_game_logs:
+                game_logs.append({
+                    'Player': player.player_name,
+                    'Game Date': log.game_date,
+                    'Assists': log.ast,
+                    'Turnovers': log.tov
+                })
+    return game_logs
+# -----------Graph Functions-----------------
+def create_assist_leaders_bar_graph(assist_leaders):
+    # Convert query results to a DataFrame
+    df = pd.DataFrame(assist_leaders, columns=['Player Name', 'Total Assists'])
+
+    # Create a bar graph
+    fig = px.bar(df, x='Player Name', y='Total Assists', title='NBA Assist Leaders This Season')
+
+    # Update layout for better readability
+    fig.update_layout(xaxis_tickangle=-45, plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
+    fig.update_traces(marker_color='blue')  # Optional: update marker color
+    fig.update_layout(
+                height=600,
+                xaxis=dict(tickangle=-45, tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
+                yaxis=dict(tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
+                title_font=dict(color='#BEC0C2'),
+                paper_bgcolor='rgba(0,0,0,0)',
+                plot_bgcolor='rgba(0,0,0)'
+            )
+    return fig.to_html(full_html=False)  # Convert the figure to HTML for embedding in Flask template
+def create_avg_assists_bar_plot(top_players_avg_assists):
+    # Convert the fetched data into a DataFrame
+    df = pd.DataFrame(top_players_avg_assists, columns=['Player Name', 'Average Assists Per Game'])
+
+    # Create a bar plot
+    fig = px.bar(df, x='Player Name', y='Average Assists Per Game', title='Top 10 Players by Average Points Per Game in 2023-24 Season')
+   
+    fig.update_layout(
+                height=600,
+                xaxis=dict(tickangle=-45, tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
+                yaxis=dict(tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
+                title_font=dict(color='#BEC0C2'),
+                paper_bgcolor='rgba(0,0,0,0)',
+                plot_bgcolor='rgba(0,0,0)'
+            )
+
+    # Convert plot to HTML for Flask rendering
+    return fig.to_html(full_html=False)
+
+def create_assists_turnovers_boxplot(game_logs):
+    df = pd.DataFrame(game_logs)
+    
+    # Plot assists
+    fig = px.box(df, x='Player', y='Assists', color='Player',
+                 title='Game-by-Game Assist and Turnover Distribution for Top 10 Assist Leaders in 2023-24')
+                 
+    # Overlay turnovers on the same plot
+    fig.add_trace(px.box(df, x='Player', y='Turnovers', color='Player').data[0])
+    
+    fig.update_layout(xaxis={'categoryorder':'total descending'}, boxmode='group')
+    fig.update_layout(
+            height=600,
+            xaxis=dict(tickangle=-45, tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
+            yaxis=dict(tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
+            title_font=dict(color='#BEC0C2'),
+            paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(0,0,0)'
+        )
+    return fig
 
 def create_app():
     app = Flask(__name__)
@@ -24,83 +136,21 @@ def create_app():
     @app.route('/haliburton')
     def haliburton():
         try:
-            current_year = datetime.now().year
-            season_str_this_year = f"{current_year-1}-{str(current_year)[-2:]}"
-            season_str_40_years_ago = f"{current_year-41}-{str(current_year-40)[-2:]}"
-
-            # Fetch this year's assist leaders
-            this_year_stats = db.session.query(SeasonStats, Player, Season).join(Player).join(Season).filter(Season.year == season_str_this_year).all()
-
-            # Fetch past 40 years' assist leaders
-            past_40_years_stats = db.session.query(SeasonStats, Player, Season).join(Player).join(Season).filter(Season.year.between(season_str_40_years_ago, season_str_this_year)).all()
-
-            # Prepare data for this year's plot
-            data_this_year = [{'Player': stats.Player.player_name, 'Assists': stats.SeasonStats.assists, 'Season': stats.Season.year} for stats in this_year_stats]
-            df_this_year = pd.DataFrame(data_this_year)
-
-            # Prepare data for past 40 years' plot
-            data_past_40_years = [{'Player': stats.Player.player_name, 'Assists': stats.SeasonStats.assists, 'Season': stats.Season.year} for stats in past_40_years_stats]
-            df_past_40_years = pd.DataFrame(data_past_40_years)
-
-            # Generate Plotly figures
-            fig_this_year = px.box(df_this_year, x='Player', y='Assists', title="Assist Leaders of This Year", color='Season')
-            fig_past_40_years = px.box(df_past_40_years, x='Player', y='Assists', title="Assist Leaders of the Past 40 Years", color='Season')
-
-            # Update layout for transparency and label rotation
-            fig_this_year.update_layout(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)', xaxis_tickangle=-90)
-            fig_past_40_years.update_layout(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)', xaxis_tickangle=-90)
-
-            # Convert to HTML
-            graph_html_this_year = fig_this_year.to_html(full_html=False)
-            graph_html_past_40_years = fig_past_40_years.to_html(full_html=False)
- # Crea te Plotly figure
-            # fig = px.bar(x=names, y=assists, title="Top 20 NBA Assist Leaders")
-            #     # Update layout for transparent background
-            # fig.update_layout(
-            #     plot_bgcolor='rgba(0,0,0,0)',
-            #     paper_bgcolor='rgba(0,0,0,0)',
-            #     xaxis_tickangle=-90  # Rotate x-axis labels to vertical
-            # )
-            # # Convert to HTML
-            # graph_html = fig.to_html(full_html=False)
-            # fig2.update_layout(
-            #     height=600,
-            #     xaxis=dict(tickangle=-45, tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
-            #     yaxis=dict(tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
-            #     title_font=dict(color='#BEC0C2'),
-            #     paper_bgcolor='rgba(0,0,0,0)',
-            #     plot_bgcolor='rgba(0,0,0)'
-            # )
-
-            # # Convert to HTML
-            # graph_html2 = fig2.to_html(full_html=False)
-
+            assist_leaders = fetch_current_season_assist_leaders()
+            graph_html = create_assist_leaders_bar_graph(assist_leaders)
+            top_players_avg_assists = fetch_top_10_players_avg_assists_2023_24()
+            graph_html2 = create_avg_assists_bar_plot(top_players_avg_assists)
             
-            return render_template('haliburton.html',plot_html = graph_html_this_year, plot_html2=graph_html_past_40_years)
+            
+            return render_template('haliburton.html', graph_html=graph_html, graph_html2=graph_html2)
+            # Convert to HTML
+            # graph_html_this_year = fig_this_year.to_html(full_html=False)
+     
+
         except Exception as e:
             # Handle errors like file not found, invalid format etc.
             return str(e), 500
     return app
-
-# def create_box_plot(df, x_column, y_column, title):
-#     # Calculate average assists per player
-#     avg_assists = df.groupby(x_column)[y_column].mean().round(2)
-
-#     # Create a new column for modified x-axis labels
-#     df['x_labels'] = df[x_column].apply(lambda x: f"{x} (Avg: {avg_assists[x]} AST/G)")
-
-#     # Create the box plot
-#     fig = px.box(df, x='x_labels', y=y_column, title=title)
-#     fig.update_traces(marker_color='#FDBB30', line_color='#FDBB30')
-#     fig.update_layout(
-#         height=600,
-#         xaxis=dict(tickangle=-45, tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
-#         yaxis=dict(tickfont=dict(size=10, color='#BEC0C2'), title_font=dict(color='#BEC0C2')),
-#         title_font=dict(color='#BEC0C2'),
-#         paper_bgcolor='rgba(0,0,0,0)',
-#         plot_bgcolor='rgba(0,0,0,0)'
-#     )
-#     return fig.to_html(full_html=False)
 
 
 
